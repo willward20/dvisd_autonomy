00a8ce08246650108dfe421e7dbf48151b7605ac
Will Ward <willward1912@gmail.com>
2026-02-27 14:00:39 -0600

    Code done

--- scripts/lesson4/jiggle_ir_lane_keeping.py
+++ scripts/lesson4/jiggle_ir_lane_keeping.py
@@ -23,8 +23,7 @@ def main(config_path):
 
     BLACK_STATE = 1
 
-    # FORWARD_SPEED = 1640
-    FORWARD_SPEED = 1640
+    FORWARD_SPEED = 1637
     STOP_SPEED = 1620
     LEFT_TURN = 80
     RIGHT_TURN = 120
