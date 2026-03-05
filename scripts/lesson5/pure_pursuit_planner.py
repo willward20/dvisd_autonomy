b2dd2f44636a392b0717f9540dddaa8c30efe9da
d31003 <d31003@gmail.com>
2026-03-05 16:23:40 -0600

    student template with instructions

--- scripts/lesson5/pure_pursuit_planner.py
+++ scripts/lesson5/pure_pursuit_planner.py
@@ -1,6 +1,15 @@
 from dvisd_autonomy.pure_pursuit.navigator import AutonomousNavigator
 from dvisd_autonomy.control.control import Control
 
+### Tune the parameters yourself ###
+esc_neutral_us = 1580
+esc_forward_us = 1650
+wheelbase = 0.2
+lookahead = 0.4
+resolution = 0.05
+target_pulse_us = 1580
+####################################
+
 # 1. Define your path: A simple 2-meter square
 square_waypoints = [
     [0.0, 0.0],
@@ -13,8 +22,8 @@ square_waypoints = [
 # 2. Initialize your specific hardware config
 rc_hardware = Control(
     freq_hz=100,
-    esc_neutral_us=1580,
-    esc_forward_us=1650, # Slow crawl for testing
+    esc_neutral_us=esc_neutral_us,
+    esc_forward_us=esc_forward_us, # Slow crawl for testing
     neutral_angle=100,
     steering_min=50,
     steering_max=140
@@ -24,8 +33,8 @@ rc_hardware = Control(
 nav = AutonomousNavigator(
     rc_hardware,
     square_waypoints,
-    wheelbase=0.2,
-    lookahead=0.4,
-    resolution=0.05
+    wheelbase=wheelbase,
+    lookahead=lookahead,
+    resolution=resolution
     )
-nav.run(target_pulse_us=1580)
+nav.run(target_pulse_us=target_pulse_us)
